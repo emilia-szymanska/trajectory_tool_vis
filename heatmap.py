6a8bad73673e7f8d1e1909d943269210051d7a32
emilia-szymanska <emilia.szymanska.eka@gmail.com>
2023-06-05 11:33:41 +0200

    Combine the occupancy grid with tfs from csv file

--- heatmap.py
+++ heatmap.py
@@ -2,10 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from shapely.geometry import box, Polygon
 import numpy as np
+import csv
+import tf
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import PoseArray, Pose, Quaternion, Point
+import matplotlib.pyplot as plt
+
+def read_csv(filename):
+    csvfile = open(filename)
+    csvreader = csv.reader(csvfile)
+    header = next(csvreader)
+    rows = []
+    for row in csvreader:
+        tmp = {}
+        for key, val in zip(header, row):
+            tmp[key] = val
+        rows.append(tmp)
+    current = []
+    for row in rows:
+        p = (row['x'], row['y'], row['z'])
+        q = (row['qx'], row['qy'], row['qz'], row['qw'])
+        pose = tf.transformations.concatenate_matrices(tf.transformations.translation_matrix(p),
+            tf.transformations.quaternion_matrix(q))
+        current.append(pose)
+    return current
 
 def update_grid(grid, robot_width, robot_length, robot_position, robot_orientation):
-
-    # Define robot as a rectangle
     robot_corners = np.array([
         [-robot_length/2, -robot_width/2],
         [robot_length/2, -robot_width/2],
@@ -35,6 +57,15 @@ def update_grid(grid, robot_width, robot_length, robot_position, robot_orientati
             if robot.intersects(cell):
                 grid[j, i] += 1
 
+def plot_grid(grid):
+    plt.figure(figsize=(6,6)) # size of the figure
+    plt.imshow(grid, cmap='hot', interpolation='nearest')
+    plt.colorbar(label='Value') # Display a colorbar
+    plt.xlabel('X') # Label for the x-axis
+    plt.ylabel('Y') # Label for the y-axis
+    plt.title('Heatmap') # Title of the heatmap
+    plt.show()
+
 grid_cell_size = 0.1
 grid_size = np.array([50.0, 50.0])  # size of the grid (width, height)
 num_cells = (grid_size / grid_cell_size).astype(int)
@@ -42,15 +73,29 @@ grid = np.zeros(num_cells, dtype=int)
 
 robot_width = 1.0  # width of the robot
 robot_length = 2.0  # length of the robot
-robot_position = np.array([5.0, 5.0])  # position of the robot (center of rectangle)
-robot_orientation = np.pi/3  # orientation of the robot in radians (counter-clockwise from x-axis)
 
-update_grid(grid, robot_width, robot_length, robot_position, robot_orientation)
+p0 = (5, 40, 0.0)
+q0 = (0.0, 0.0, -0.125, 1)
+converted = tf.transformations.concatenate_matrices(
+        tf.transformations.translation_matrix(p0),
+        tf.transformations.quaternion_matrix(q0)
+    )
 
-plt.figure(figsize=(6,6)) # size of the figure
-plt.imshow(grid, cmap='hot', interpolation='nearest')
-plt.colorbar(label='Value') # Display a colorbar
-plt.xlabel('X') # Label for the x-axis
-plt.ylabel('Y') # Label for the y-axis
-plt.title('Heatmap') # Title of the heatmap
-plt.show()
+poses = read_csv("smoothed.csv")
+
+global_poses = PoseArray()
+for transform in poses:
+    converted = converted @ transform
+    topush = Pose()
+    topush.orientation = Quaternion(*tf.transformations.quaternion_from_matrix(converted))
+    topush.position = Point(*tf.transformations.translation_from_matrix(converted))
+    global_poses.poses.append(topush)
+
+for pose in global_poses.poses:
+    position = np.array([pose.position.x, pose.position.y])
+    quaternion = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
+    euler = euler_from_quaternion(quaternion)
+    yaw = euler[2]
+    update_grid(grid, robot_width, robot_length, position, yaw)
+
+plot_grid(grid)
